5ed50a4ef11beb439981b0957b5a11bf0c3dea19
roisuperstar <trantuanit95@gmail.com>
2020-09-17 23:59:05 +0700

    lab4

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int x, y;
+	char a = 'y';
+	x=y=0;
+	if (a=='y')
+	{
+		x+=5;
+		printf("the number are %d and \t%d", x, y);
+	}
+}
---
+++ main4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+ char c;
+ printf("Please enter a character : ");
+ scanf("%c",&c);
+ if (c >= 'A' && c <= 'Z')
+ printf("Lowercase character = %c", c + 'a' - 'A');
+ else
+ printf("Character Entered is = %c",c);
+}
